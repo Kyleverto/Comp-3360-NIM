1ff81f4c3684b7f0f66a0912b0754bd3d971172b
csipe4 <csipe4@harding.edu>
2026-04-27 14:52:37 -0500

    Game Logic finished
    Core game logic done still bugs to fix.

--- gameBoard.cpp
+++ gameBoard.cpp
@@ -1,18 +1,22 @@
 public Class GameBoard {
-private:
-	int numPiles = 0;
-	int piles[];
 
-public :
 	GameBoard(char[]& board) {
 		createPiles(board);
 	}
+
+	int numPiles = 0;
+	int piles[];
 
 	void createPiles(char[]& board) {
 		if (stoi(board[0]) >= 3 && stoi(board[0]) <= 9)  {
 			numPiles = stoi(board[0]);
 			for (int i = 1; i < char[].length; i = i + 2) {
-				piles.push(stoi(strcat(board[i], board[i + 1])));
+				if (stoi(strcat(board[i], board[i + 1])) <= 20 && stoi(strcat(board[i], board[i + 1])) > 0) {
+					piles.push(stoi(strcat(board[i], board[i + 1])));
+				}
+				else {
+					numPiles = 0;
+				}
 			}
 		}
 	}
@@ -30,6 +34,17 @@ public :
 
 		piles[fromPile] -= numToRemove;
 		return 0; // Valid move: Update the game board
+	}
+
+	char checkWin() {
+		for (int i = 0; i < piles.length; i++) {
+			if (piles[i] > 0) {
+				return 'C'
+			}
+			else {
+				return 'W';
+			}
+		}
 	}
 	
 
